60cce07af95de705cb95ce6c5ca9a33a122d28f8
Brandon Pramann <bpramann1@gmail.com>
2019-02-14 10:01:31 -0600

    Add SaveGame Class

---
+++ Minesweeper/SaveGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    class SaveGame
+    {
+        private Form saveGameForm;
+        private GameMap GameMapSender;
+
+        public SaveGame(GameMap sender)
+        {
+            GameMapSender = sender;
+            CreateSaveGameDialog();
+        }
+
+        public void CreateSaveGameDialog()
+        {
+            saveGameForm = new Form();
+
+        }
+    }
+}
