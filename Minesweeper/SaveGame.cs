d0bcc19ef603716d02f1c06a23d4e0056c5c9819
Brandon Pramann <bpramann1@gmail.com>
2019-02-14 10:39:09 -0600

    Added Exit Functionality to SaveGame

--- Minesweeper/SaveGame.cs
+++ Minesweeper/SaveGame.cs
@@ -9,11 +9,21 @@ namespace Minesweeper
 {
     class SaveGame
     {
+
         private Form saveGameForm;
         private GameMap GameMapSender;
 
-        public SaveGame(GameMap sender)
+        public enum ActionsAfterSave
         {
+            Nothing,
+            Restart,
+            Exit
+        }
+        private ActionsAfterSave actionAfterSave;
+
+        public SaveGame(GameMap sender, ActionsAfterSave ActionAfterSave)
+        {
+            actionAfterSave = ActionAfterSave;
             GameMapSender = sender;
             CreateSaveGameDialog();
         }
@@ -21,6 +31,36 @@ namespace Minesweeper
         public void CreateSaveGameDialog()
         {
             saveGameForm = new Form();
+
+
+        }
+        public void SaveGameDialogDone()
+        {
+            switch (actionAfterSave)
+            {
+                case ActionsAfterSave.Restart:
+                    System.Diagnostics.Process.Start(Application.ExecutablePath);
+                    Environment.Exit(0);
+                    break;
+                case ActionsAfterSave.Exit:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        private void ExitButtonClicked(object sender, FormClosingEventArgs e)
+        {
+            DialogResult shouldSave = MessageBox.Show("Are you sure that you do not want to save?", "Save", MessageBoxButtons.YesNo);
+            if (shouldSave == DialogResult.Yes)
+            {
+                SaveGameDialogDone();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
 
         }
     }
