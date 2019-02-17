296b95d15d2d6489c2e2c44b3e289bc11db1fce1
Brandon Pramann <bpramann1@gmail.com>
2019-02-17 12:12:06 -0600

    Add Documentation to SaveTypeDialog and DeleteSaveDialog

--- Minesweeper/SaveTypeDialog.cs
+++ Minesweeper/SaveTypeDialog.cs
@@ -6,12 +6,21 @@ using System.Threading.Tasks;
 
 namespace Minesweeper
 {
+    /// <summary>
+    /// This class is here to be inherited by the SaveGame, LoadGame, and Delete game class. This class is basically virtual.
+    /// </summary>
     class SaveTypeDialog
     {
+        /// <summary>
+        /// This creates an instance of the Save Type Class. This should not be used.
+        /// </summary>
         public SaveTypeDialog()
         {
 
         }
+        /// <summary>
+        /// This method is a virtual method that should be overwritten by all the children of this class.
+        /// </summary>
         public virtual void ButtonClicked()
         {
 
