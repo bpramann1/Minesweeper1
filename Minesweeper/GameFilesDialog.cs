43e7ba14b3f17a3678533fc1f42e7511361a78d4
Adam Soelter <adamdlsoelter@gmail.com>
2019-02-17 15:15:29 -0600

    Documented file dialogue class

--- Minesweeper/GameFilesDialog.cs
+++ Minesweeper/GameFilesDialog.cs
@@ -7,6 +7,9 @@ using System.Windows.Forms;
 
 namespace Minesweeper
 {
+    /// <summary>
+    /// Manages a file menu for the player 
+    /// </summary>
     class GameFilesDialog
     {
 
@@ -22,6 +25,9 @@ namespace Minesweeper
         private SaveTypeDialog senderObject;
         private string callType;
 
+        /// <summary>
+        /// Contains a value that dictates what actions need to be taken after the dialog is closed 
+        /// </summary>
         public enum ActionsAfterDialog
         {
             Nothing,
@@ -30,6 +36,24 @@ namespace Minesweeper
         }
         private ActionsAfterDialog actionAfterDialog;
 
+        /// <summary>
+        /// Constructs a new files dialog 
+        /// </summary>
+        /// <param name="ActionAfterDialog">
+        /// An enum that dictates the action to be taken after the dialog is over 
+        /// </param>
+        /// <param name="gameMapSender">
+        /// The game map that called the dialog 
+        /// </param>
+        /// <param name="sender">
+        /// The type of action to be taken (save, load, or delete)
+        /// </param>
+        /// <param name="finishString">
+        /// The string to be displayed at the end of the game 
+        /// </param>
+        /// <param name="buttonText">
+        /// The text to be displayed by the button 
+        /// </param>
         public GameFilesDialog(ActionsAfterDialog ActionAfterDialog, GameMap gameMapSender, SaveTypeDialog sender, string finishString, string buttonText)
         {
             callType = finishString;
@@ -40,16 +64,31 @@ namespace Minesweeper
             saveGameButton.Text = buttonText;
         }
 
+        /// <summary>
+        /// Gets the string to be displayed when saved 
+        /// </summary>
+        /// <returns>
+        /// The string to be displayed when saved 
+        /// </returns>
         public string getSaveString()
         {
             return saveString;
         }
 
+        /// <summary>
+        /// Sets the dialogue to either be done or not done 
+        /// </summary>
+        /// <param name="value">
+        /// The value to set dialogDone to 
+        /// </param>
         public void setDialogDone(bool value)
         {
             dialogDone = value;
         }
 
+        /// <summary>
+        /// Creates a new dialog 
+        /// </summary>
         private void CreateDialog()
         {
             GameFilesForm = new Form();
@@ -84,6 +123,10 @@ namespace Minesweeper
             savedGamesList.MouseDoubleClick += new MouseEventHandler(SaveListItemDoubleClicked);
             PopulateSaveList();
         }
+
+        /// <summary>
+        /// Performs the neccessary actions to be done after the dialog is closed. 
+        /// </summary>
         public void SaveGameDialogDone()
         {
             switch (actionAfterDialog)
@@ -104,6 +147,15 @@ namespace Minesweeper
             }
         }
 
+        /// <summary>
+        /// Makes sure the user actually wants to exit if the game is still going and then closes the game. 
+        /// </summary>
+        /// <param name="sender">
+        /// The object that called this function
+        /// </param>
+        /// <param name="e">
+        /// Contains information about the closing event 
+        /// </param>
         private void ExitButtonClicked(object sender, FormClosingEventArgs e)
         {
             if (!dialogDone)
@@ -121,6 +173,16 @@ namespace Minesweeper
             }
 
         }
+
+        /// <summary>
+        /// Varifies that the user has entered a correct name for the save file and then saves the name of it  
+        /// </summary>
+        /// <param name="sender">
+        /// The object that called the function
+        /// </param>
+        /// <param name="e">
+        /// Contains information about the event 
+        /// </param>
         private void ButtonClicked(object sender, EventArgs e)
         {
             if (selectedGameTextbox.Text != "")
@@ -140,6 +202,16 @@ namespace Minesweeper
                 MessageBox.Show("The name of your save must start with a letter");
             }
         }
+
+        /// <summary>
+        /// Determines if letter is a letter 
+        /// </summary>
+        /// <param name="letter">
+        /// The char to be tested for being a letter 
+        /// </param>
+        /// <returns>
+        /// True if letter is a letter, false otherwise 
+        /// </returns>
         private bool IsLetter(char letter)
         {
             if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
@@ -153,6 +225,9 @@ namespace Minesweeper
 
         }
 
+        /// <summary>
+        /// Populates the save list 
+        /// </summary>
         public void PopulateSaveList()
         {
             string path = Application.StartupPath;
@@ -176,6 +251,16 @@ namespace Minesweeper
             }
 
         }
+
+        /// <summary>
+        /// Populates the selectedGameTextbox with the name of the game to open 
+        /// </summary>
+        /// <param name="sender">
+        /// The object that called this function
+        /// </param>
+        /// <param name="e">
+        /// Contains information about the event that caused this function to be called 
+        /// </param>
         private void SaveListItemSingleClicked(object sender, MouseEventArgs e)
         {
             if (savedGamesList.SelectedItem != null)
@@ -184,6 +269,16 @@ namespace Minesweeper
             }
 
         }
+
+        /// <summary>
+        /// Prompts the user to make sure they want to overwrite a previous save and then overwrites the save they double clicked 
+        /// </summary>
+        /// <param name="sender">
+        /// Contains information about the object that called this function
+        /// </param>
+        /// <param name="e">
+        /// Contains information about the event that caused this function to be called 
+        /// </param>
         private void SaveListItemDoubleClicked(object sender, MouseEventArgs e)
         {
             if (savedGamesList.SelectedItem != null)
@@ -193,6 +288,13 @@ namespace Minesweeper
                 senderObject.ButtonClicked();
             }
         }
+
+        /// <summary>
+        /// Determines whether the inputted name for the save already exists 
+        /// </summary>
+        /// <returns>
+        /// True if the name already exists, false otherwise
+        /// </returns>
         public bool saveStringAlreadyExists()
         {
             foreach (object item in savedGamesList.Items)
